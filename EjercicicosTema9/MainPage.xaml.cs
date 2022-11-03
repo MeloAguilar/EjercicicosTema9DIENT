71bb935a5376d6a8a7a0679bf1fcecbcd5bed100
MeloAguilar <carmelo.aguilar@iesnervion.es>
2022-11-03 20:30:06 +0100

    Agregar archivos de proyecto.

---
+++ EjercicicosTema9/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿namespace EjercicicosTema9;
+
+public partial class MainPage : ContentPage
+{
+
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void entry_TextChanged(object sender, TextChangedEventArgs e)
+	{
+		if (entry.Text == "")
+		{
+			entry.Text="0";
+		}
+		else
+		{
+			try
+			{
+				var num = Convert.ToDouble(entry.Text);
+				var numInt = Convert.ToInt32(num);
+				if (numInt >= 100)
+				{
+					entry.Text = "99";
+				}
+				else 
+				{
+					entry.Text = numInt.ToString();
+				}
+
+			}
+			catch (Exception tumare)
+			{
+				entry.Text = "50";
+			}
+
+
+		}
+	}
+}
+
